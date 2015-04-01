3f21563aaea9c167af363142bc541e9ccaca1410
richardison <richard.ison@carleton.ca>
2015-04-01 15:05:36 -0400

    Initial Commit

---
+++ InstaRep-Client/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaRep-Client
+//
+//  Created by Richard Ison on 2015-04-01.
+//  Copyright (c) 2015 Richard Ison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaRep-Client/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InstaRep-Client
+//
+//  Created by Richard Ison on 2015-04-01.
+//  Copyright (c) 2015 Richard Ison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
